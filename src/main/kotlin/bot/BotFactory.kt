817723828a1422a0b985f23bb009a83fabe2de00
vjgarciag96 <vjgarcia@tuenti.com>
2019-08-18 00:57:32 +0200

    Create factory to instantiate library's bot

---
+++ src/main/kotlin/bot/BotFactory.kt
@@ -0,0 +1,13 @@
+package bot
+
+import bot.commands.*
+import me.ivmg.telegram.Bot
+import me.ivmg.telegram.dispatcher.command
+
+fun get(botToken: String, vararg commands: BotCommand): Bot = Bot.Builder()
+    .apply {
+        this.token = botToken
+        this.updater.dispatcher.apply {
+            commands.forEach { command(it.name, it::action) }
+        }
+    }.build()
--- src/main/kotlin/bot/IoC.kt
+++ src/main/kotlin/bot/IoC.kt
@@ -10,21 +10,21 @@ const val BOT_TOKEN_CONFIG_FIELD_KEY = "bot-token-property"
 
 val botModule = module {
     single {
-        MyBot(Bot.Builder().apply {
-            this.token = getProperty(BOT_TOKEN_CONFIG_FIELD_KEY)
-            this.updater.dispatcher.apply {
-                val startCommand: StartCommand = get()
-                val goneForeverTShirtsCommand: GoneForeverTShirtsCommand = get()
-                val subscribeCommand: SubscribeCommand = get()
-                val unsubscribeCommand: UnsubscribeCommand = get()
-                val helpCommand: HelpCommand = get()
-                command(startCommand.name, startCommand::action)
-                command(goneForeverTShirtsCommand.name, goneForeverTShirtsCommand::action)
-                command(subscribeCommand.name, subscribeCommand::action)
-                command(unsubscribeCommand.name, unsubscribeCommand::action)
-                command(helpCommand.name, helpCommand::action)
-            }
-        }.build())
+        val startCommand: StartCommand = get()
+        val goneForeverTShirtsCommand: GoneForeverTShirtsCommand = get()
+        val subscribeCommand: SubscribeCommand = get()
+        val unsubscribeCommand: UnsubscribeCommand = get()
+        val helpCommand: HelpCommand = get()
+
+        val bot = bot.get(
+            getProperty(BOT_TOKEN_CONFIG_FIELD_KEY),
+            startCommand,
+            goneForeverTShirtsCommand,
+            subscribeCommand,
+            unsubscribeCommand,
+            helpCommand
+        )
+        MyBot(bot)
     }
 }
 
