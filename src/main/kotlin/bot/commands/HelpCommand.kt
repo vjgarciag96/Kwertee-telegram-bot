32d8d0a87ac72e5b1014c47c487fab69d80e1f43
vjgarciag96 <vjgarcia@tuenti.com>
2019-08-24 22:17:19 +0200

    Update help command

--- src/main/kotlin/bot/commands/HelpCommand.kt
+++ src/main/kotlin/bot/commands/HelpCommand.kt
@@ -10,7 +10,7 @@ class HelpCommand : BotCommand {
     override fun action(bot: Bot, update: Update, args: List<String>) {
         update.message?.let {
             bot.sendMessage(it.chat.id, "The bot's way of working is really simple. " +
-                "To watch the currently available 'gone forever' t-shirts, run the /goneforever command. " +
+                "To watch the currently available promoted t-shirts, run the /promoted command. " +
                 "To receive updates every day, run /subscribe. " +
                 "Once subscribed, if you want to stop receiving notifications, run the /unsubscribe command. " +
                 "And that's all, have fun!! :)")
