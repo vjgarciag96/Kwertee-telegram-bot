5f7802c358bcb05a30f4c9d7bd9241fe8a3b12d5
vjgarciag96 <vjgarcia@tuenti.com>
2019-08-25 12:55:27 +0200

    Update unsubscribe command

--- src/main/kotlin/bot/commands/UnsubscribeCommand.kt
+++ src/main/kotlin/bot/commands/UnsubscribeCommand.kt
@@ -17,8 +17,8 @@ class UnsubscribeCommand(
 
             if (unsubscribeResult) {
                 bot.sendMessage(it.chat.id, "You'll stop receiving notifications " +
-                    "about Qwertee 'gone forever' t-shirts. If you want to check " +
-                    "the currently available 'gone forever' t-shirts, run /goneforever command. If you want to " +
+                    "about Qwertee promoted t-shirts. If you want to check " +
+                    "the currently available promoted t-shirts, run /promoted command. If you want to " +
                     "restart receiving the t-shirt updates again, run the /subscribe command")
             } else {
                 bot.sendMessage(it.chat.id, "You are not subscribed to updates, so you won't receive them")
